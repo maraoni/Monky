1d4a02747f8690cf053d5e80c5452d8551d55917
maraon99 <90472204+maraon99@users.noreply.github.com>
2025-01-31 16:18:28 +0100

    Physics engine encapsulation

--- MonkyPhysics/MonkyPhysics.cpp
+++ MonkyPhysics/MonkyPhysics.cpp
@@ -15,41 +15,25 @@ namespace Banana
 
 	void MonkyPhysics::Simulate(const float& aDeltaTime)
 	{
-		std::vector<Collider*> cols;
+		//Just setting the positions of the colliders to match the visual
+		std::vector<Collider*> cols = UpdatePhysicsScene();
 
-		for (GameObject* c : myEngine->GetGameObjects())
-		{
-			Collider* col = c->GetCollider();
-			if (col != nullptr)
-			{
-				col->position = c->GetVirtual()->Position;
-				cols.push_back(col);
-			}
-		}
+		//Checking for any intersections and storing their data in a vector of collisions
+		std::vector<Collision> collisions = CheckIntersections(cols);
 
-		BeginStep(cols, aDeltaTime);
+		//As a result of those collisions, what should happen?
+		HandleCollisions(collisions);
 
-		std::vector<Collision> collisions;
-		for (Collider* c1 : cols)
-		{
-			for (Collider* c2 : cols)
-			{
-				if (c1 != c2)
-				{
-					if (CheckIntersect(c1, c2))
-					{
-						Collision collision;
-						collision.col1 = c1;
-						collision.col2 = c2;
-					}
-				}
-			}
-		}
+		//At the moment this is only applying gravity to my colliders since i have no
+		//calculations for linear and angular velocity based on collisions.
+		//This should ideally be in HandleCollisions
+		ApplyVelocity(cols, aDeltaTime);
 
-		EndStep();
+		//Making sure that the visuals of the colliders is aligned with the colliders
+		UpdateVisuals();
 	}
 
-	void MonkyPhysics::BeginStep(std::vector<Collider*> colliders, const float& dt)
+	void MonkyPhysics::ApplyVelocity(std::vector<Collider*> colliders, const float& dt)
 	{
 		for (Collider* c : colliders)
 		{
@@ -63,13 +47,55 @@ namespace Banana
 
 	void MonkyPhysics::HandleCollisions(std::vector<Collision> collisions)
 	{
-		for (Collision c : collisions) 
+		for (Collision c : collisions)
 		{
 
 		}
 	}
 
-	void MonkyPhysics::EndStep()
+	std::vector<Collider*> MonkyPhysics::UpdatePhysicsScene()
+	{
+		std::vector<Collider*> cols;
+		for (GameObject* c : myEngine->GetGameObjects())
+		{
+			Collider* col = c->GetCollider();
+			if (col != nullptr)
+			{
+				col->position = c->GetVirtual()->Position;
+				cols.push_back(col);
+			}
+		}
+		return cols;
+	}
+
+	std::vector<Collision> MonkyPhysics::CheckIntersections(std::vector<Collider*> colliders)
+	{
+		std::vector<Collision> collisions;
+		for (Collider* c1 : colliders)
+		{
+			for (Collider* c2 : colliders)
+			{
+				if (c1 != c2)
+				{
+					if (CheckIntersect(c1, c2))
+					{
+						std::cout << "Hey we are colliding" << std::endl;
+						std::cout << "!" << std::endl;
+
+						//This will eventually become the return value of check intersect
+						Collision collision;
+						collision.col1 = c1;
+						collision.col2 = c2;
+						collisions.push_back(collision);
+						//---------------------------------------------------------------
+					}
+				}
+			}
+		}
+		return collisions;
+	}
+
+	void MonkyPhysics::UpdateVisuals()
 	{
 		for (GameObject* c : myEngine->GetGameObjects())
 		{
--- MonkyPhysics/MonkyPhysics.h
+++ MonkyPhysics/MonkyPhysics.h
@@ -30,9 +30,11 @@ namespace Banana
 		MonkyPhysics(Engine::MonkyEngine* aEngine);
 		void Simulate(const float& aDeltaTime);
 
-		void BeginStep(std::vector<Collider*> colliders, const float& dt);
+		void ApplyVelocity(std::vector<Collider*> colliders, const float& dt);
 		void HandleCollisions(std::vector<Collision> collisions);
-		void EndStep();
+		std::vector<Collider*> UpdatePhysicsScene();
+		std::vector<Collision> CheckIntersections(std::vector<Collider*> colliders);
+		void UpdateVisuals();
 
 	private:
 
